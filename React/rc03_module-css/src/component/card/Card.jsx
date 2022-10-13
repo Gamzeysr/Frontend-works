b446cf542af059ccbe13a0f3c987202fd2567c69
Gamze <gamzeyasar35@icloud.com>
2022-10-13 16:01:21 +0300

    img

--- React/rc03_module-css/src/component/card/Card.jsx
+++ React/rc03_module-css/src/component/card/Card.jsx
@@ -7,9 +7,9 @@ import CardStyle from './card.module.css';
 const Card = ({ language, btn, img }) => {
     // console.log({props});
     return (
-        <div className={CardStyle.title} >
-            <h1 >{language}</h1>
-            <img className="images" src={img} alt="img" />
+        <div  >
+            <h1 className={CardStyle["title"]}>{language}</h1>
+            <img className={CardStyle["images"]} src={img} alt="img" />
 
 
             <Buton btn={btn} />
