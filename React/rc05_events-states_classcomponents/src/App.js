b52ed755ec4e89738cc0b637d2e584d3b6e1e1ba
Gamze <gamzeyasar35@icloud.com>
2022-10-18 10:16:53 +0300

    usestatehook

--- React/rc05_events-states_classcomponents/src/App.js
+++ React/rc05_events-states_classcomponents/src/App.js
@@ -1,14 +1,15 @@
-import Events from "./components/1-events/Events";
+mport Events from "./components/1-events/Events";
+import Counter from "./components/2-classComponent/Counter";
+import UseStateCounter from "./components/3-useStateHook/UseStateCounter";
 
 function App() {
   return (
-    <div className="App">
-      <Events />
-      {/* <Counter /> */}
-
-
+    <div>
+      {/* <Events />
+      <Counter />
+      // <Counter count={5} /> */}
+      <UseStateCounter />
     </div>
   );
 }
-
 export default App;
