004f5d0635b400b68480b86f9dc704d47a37b9d7
Gamze <gamzeyasar35@icloud.com>
2022-10-18 16:06:31 +0300

    Forms

---
+++ React/rc06_forms_otherevents/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Form from './components/1-forms/Form';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
