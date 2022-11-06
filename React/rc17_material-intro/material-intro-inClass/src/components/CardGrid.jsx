4019dd5823bc36c4c31c7b4301b62a9b1e564654
Gamze <gamzeyasar35@icloud.com>
2022-11-06 11:03:01 +0300

    Card and Grid

--- React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
+++ React/rc17_material-intro/material-intro-inClass/src/components/CardGrid.jsx
@@ -5,8 +5,11 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import Grid from "@mui/material/Grid";
+// Meterialdaki Grid elementini kullanmak için import ettiik 
 import data from "../data";
-
+//☝ verimiz datada oldugu için burada da kullanmk istediğimiz için buraya import ediyoruz ilk önce.
+// sOnrada render etcez yani ekrana bascaz bunun içinde iterasyon lazım 
+//! 👇 Burada ki amacımız bootstrap daki card yapılarını material da nasıl yaparız onu görücez 
 export default function CardGrid() {
   return (
     <>
@@ -14,13 +17,24 @@ export default function CardGrid() {
         CARD And Grid
       </Typography>
       <Grid container spacing={2} justifyContent="center" alignItems="center">
+        {/* Burada da import ettiğimiz grid yapısını kullandık bu grid yapısı container olan*/}
         {data.map((card) => {
+          // ☝ jsx içerisinde bir statemnt yazmak istiyorsak süslü içinde kullanıyorduk bu yüzden süslü içine aldık .
+          //! datanın içinde dolasıcagımızdan {data.map} dedik.sonrasında da arrow func inimizi kullanıyoruz 
           const { id, text, img, name } = card;
+          // ☝ datanın içinde kullanacak olduklarımızı buraya cektim ekranda bunları görmek istiyorum diye dest yaparak 
           return (
+            // ☝ benim yaptıgım arrow func return u
             <Grid item xs={12} sm={6} md={4} key={id}>
+              {/* col-row mantıgı için xs12 diyerek  her bir resim tek tek alt alta görünsün dedik.
+              sm 6 da resim ikişerli görünsün dedik.
+              md ekranda yani geniş medium ekranda resimler 3 tane görünsün dedik  */}
               <Card>
                 <CardActionArea>
-                  <CardMedia component="img" image={img} alt="img" />
+                  <CardMedia
+                    component="img"
+                    image={img}
+                    alt="img" />
                   <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                       {name}
--- React/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
+++ React/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -16,16 +16,20 @@ const TextFieldComp = () => {
         </Typography>
 
         <Box sx={{ mt: 4, textAlign: "center" }}>
+          {/* ☝Burada ki text-align center i submit butonu ortalansın diye verdik yani ortak olan divimize verdik bi nevi. */}
           <TextField
             margin="dense"
             id="email"
             label="Email"
             placeholder="Enter your email"
             fullWidth
+            //☝ email butonu sayfa boyu kaplasın diye fullWidth metodunu kullanduk.Yani container ne kadarsa %100 ü kadar kapa dedik.
             error={error}
+            //☝ Buradaki error propsu yukarıda tanımladıgım const [error,setError] = useState() e baglı olarak calısıcak 
             helperText={error && "Incorrect email format"}
+          // helper text de e diyoruz ki error varsa error true ise bize Incorret email format hata mesajı verdir.
+          // Yukarıda ki useState true false durumuna göre şekil alıyor.
           />
-
           <TextField
             margin="normal"
             id="password"
