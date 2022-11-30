f498987a0b8182b704b6ae6e1d2b7beef24b06fa
Gamze <gamzeyasar35@icloud.com>
2022-11-30 17:09:38 +0300

    Redux

---
+++ React/rc21_redux-intro/redux-intro-inClass/src/redux/reducers/counterReducer.js
@@ -0,0 +1,29 @@
+import { CLEAR, DECREMENT, INCREMENT } from "../types/counterType";
+
+const initialState = {
+  counter: 0,
+};
+
+const counterReducer = (state = initialState, action) => {
+  //* state = initialState yazmamızın sebebi state baslangıc verdik o yüzden.
+  //* belirtmesek de oluyordu ama genelde reduxde belirterek yapıyoruz. 
+  switch (action.type) {
+    case INCREMENT:
+      // artırma 👆
+      return { counter: state.counter + 1 };
+    case DECREMENT:
+      // azaltma 👆
+      return { counter: state.counter - 1 };
+    case CLEAR:
+      // silme👆
+      return initialState;
+    default:
+      return state;
+    // burada illa statin ilk halini yazmalıyız. 
+  }
+};
+export default counterReducer;
+//! Counteri artırma azaltma silme işlemlerini yapıcaz burada ki switch case yapılarıyla.
+//!✨ Burada neden BUYUK HARFLE yazıyoruz sebebi: kodlamada hiç degeri değişmeyenlere biz constıt deriz. Bir kere baslangıctta deger alıyor b-ve bir daha değişmiyor.ve bu constıtlar string se genellkile buyuk harfle yazılır.
+//* reducer lar de state ve action alıyor.reducer ın birinci parametresi her zaman statedir.İkinci parametre actiondur.✨{typeipayload}✨ şeklinde de görebiliriz.
+//* payload da dispact in gönderdiğği veri.
