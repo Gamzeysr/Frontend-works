602626612044c11478ac6107b026b992425596a5
Gamze <gamzeyasar35@icloud.com>
2023-02-08 10:48:58 +0300

    addEventListener

--- JavaScript-Advance/Js-Dom-events-2/1-events-intro/events.js
+++ JavaScript-Advance/Js-Dom-events-2/1-events-intro/events.js
@@ -4,36 +4,43 @@ console.log("********* EVENTS ********");
 //* EXAMPLE-1 (Mouse over ,Mouse out)********
 
 
-// const h1 = document.querySelector("#header h1")
-// console.log(h1);
-// // Burada hedaer id nin içindeki h1 i getir dedilk
+const h1 = document.querySelector("#header h1")
+console.log(h1);
+//1. Burada hedaer id nin içindeki h1 i getir dedilk
 
-// h1.onmouseover = function () {
-//     h1.style.color = "red";
-//     h1.style.fontWeight = "200";
-// };
+h1.onmouseover = function () {
+    h1.style.color = "red";
+    h1.style.fontWeight = "200";
+};
+//2.👆 baslıgın üzerine gelindiğinde yazsıını incelt ve rengini kırmızı yap dedik 
 
-// h1.onmouseout = () => {
-//     h1.style.color = "black";
-//     h1.style.fontWeight = "900";
-// };
+h1.onmouseout = () => {
+    h1.style.color = "black";
+    h1.style.fontWeight = "900";
+};
+//3.👆 mouse baslıgın üzerinden ayrılınca rengi siyah kalıngın da 900 olsun dedik 
+//! DİKKAT:Burada önemli bir ayrıntı mouseoutu yazmasaydım mouseun üzerine gelindiğinde kırmızı ve ınce olması olrak ayarlı oldugundan baslıgın üzerine gelince mouse kırmı ve ınce olacak ve o sekilde kalacaktı. Ama baslıkta omouse da devreye sokunca mousun uzerinden ayrılınca sıyah ve kalınlasdı.
+//? 4.method (addEventListener)
+h1.addEventListener("click", () => {
+    alert("H1 pressed");
+});
 
 // //*** EXAMPLE-2 (addEventListener()) */
 
 
-// document.getElementById("btn").addEventListener('click', function () {
-//     const input = document.querySelector("#input");
-//     if (!input.value) {
-//         alert("Please enter an item");
-//     } else {
-//         alert(`${input.value} entered`);
-//     }
+document.getElementById("btn").addEventListener('click', function () {
+    const input = document.querySelector("#input");
+    if (!input.value) {
+        alert("Please enter an item");
+    } else {
+        alert(`${input.value} entered`);
+    }
 
-// });
+});
 
 
-// // ternary hali
-// // !input.value ? alert("Please enter an item") : alert(`${input.value} entered`)
+// ternary hali
+// !input.value ? alert("Please enter an item") : alert(`${input.value} entered`)
 // //* EXAMPLE-3
 // //* -------------------------------------------------
 // const list = document.querySelectorAll(".list"); //?nodelist
