448a6abfbd5f8bf1b256499f86a365bd9f8132a5
Gamze <gamzeyasar35@icloud.com>
2023-02-08 20:00:09 +0300

    indexhtml created

--- JavaScript-Advance/Js-Dom-events-2/2-event-example/app.js
+++ JavaScript-Advance/Js-Dom-events-2/2-event-example/app.js
@@ -7,47 +7,47 @@ const deleteBtn = document.querySelector("#delete-btn");
 const langList = document.getElementById("lang-list");
 
 const newUl = document.createElement("ul");
-langList.appendChild(newUl);
+// langList.appendChild(newUl);
 
-//? addBtn event handler
-addBtn.addEventListener("click", () => {
-    if (!langInput.value) {
-        alert("Please enter a language");
-    } else {
-        newUl.innerHTML += `<li>${langInput.value}</li>`;
-        langInput.value = "";
-    }
-    langInput.focus();
-});
+// //? addBtn event handler
+// addBtn.addEventListener("click", () => {
+//     if (!langInput.value) {
+//         alert("Please enter a language");
+//     } else {
+//         newUl.innerHTML += `<li>${langInput.value}</li>`;
+//         langInput.value = "";
+//     }
+//     langInput.focus();
+// });
 
-//? deleteBtn event handler
-deleteBtn.addEventListener("click", () => {
-    newUl.childElementCount > 0
-        ? newUl.removeChild(newUl.lastElementChild)
-        : alert("There is no item to delete");
-});
+// //? deleteBtn event handler
+// deleteBtn.addEventListener("click", () => {
+//     newUl.childElementCount > 0
+//         ? newUl.removeChild(newUl.lastElementChild)
+//         : alert("There is no item to delete");
+// });
 
-//? enter key and  del key event handler
-langInput.addEventListener("keydown", (event) => {
-    // console.log(event);
-    // console.log(event.target);
-    // console.log(event.keyCode);
-    // console.log(event.code);
+// //? enter key and  del key event handler
+// langInput.addEventListener("keydown", (event) => {
+//     // console.log(event);
+//     // console.log(event.target);
+//     // console.log(event.keyCode);
+//     // console.log(event.code);
 
-    if (event.keyCode === 13) {
-        addBtn.click();
-    }
+//     if (event.keyCode === 13) {
+//         addBtn.click();
+//     }
 
-    // if (event.code === "Delete") {
-    //   deleteBtn.click();
-    // }
+//     // if (event.code === "Delete") {
+//     //   deleteBtn.click();
+//     // }
 
-    if (event.keyCode === 46) {
-        deleteBtn.click();
-    }
-});
+//     if (event.keyCode === 46) {
+//         deleteBtn.click();
+//     }
+// });
 
-//? onload event handler
-window.addEventListener("load", () => {
-    langInput.focus();
-});
+// //? onload event handler
+// window.addEventListener("load", () => {
+//     langInput.focus();
+// });
