bb3e84b8c3057b81baad377018d5a9c25e212e67
Gamze <gamzeyasar35@icloud.com>
2023-02-15 12:25:15 +0300

    okIcon deleteIcon text created

--- JavaScript-Advance/4-Todo_Solution/app.js
+++ JavaScript-Advance/4-Todo_Solution/app.js
@@ -1,2 +1,56 @@
+//? Selectors
+const addBtn = document.querySelector("#todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.querySelector("#todo-ul");
 
 
+
+
+addBtn.addEventListener("click", () => {
+    if (todoInput.value.trim() === "") {
+        alert("Please enter new todo😢");
+    } else {
+        const newTodo = {
+            id: new Date().getTime(),
+            completed: false,
+            text: todoInput.value,
+        };
+        createListElement(newTodo);
+        //!👆 Burada method olusturmamın amacı  yukarıda ki aldıgımız verileri bir fonksiyona atmak
+        //*🧨 elsein içinde methodu olusturyoruz dikkat et!!!!
+    }
+});
+//! space ile girilirse ve bosluk olursa iki türlüde alert versin diye trim() kullandıık 
+//! else de ise girilen verileri objeye attık isminede newTodo dedik
+
+//! bu da fonksıyon oluyor artık 👇OLUSTURMUS OLDGUM FONKSIYONA NEWtODO YU GÖNDERİYORUM 
+const createListElement = (newTodo) => {
+    const li = document.createElement("li");
+    // li.id = newTodo.id;
+    li.setAttribute("id", newTodo.id);
+
+    //* ok ıconu👇
+    const okIcon = document.createElement("i");
+    okIcon.setAttribute("class", "fas fa-check");
+    li.appendChild(okIcon);
+    // console.log(li);
+
+    //! todo baslıgı için bir p elementi ve yazı duguöu olsturarak li'ye bagla👇
+    const p = document.createElement("p");
+    const pTextNode = document.createTextNode(newTodo.text);
+    //* 👆kullanıcınınn girecek yazı olgundan bu idye atanacagından her inputtaki herbır deger o yuzden bu sekilde yaptık 
+    p.appendChild(pTextNode);
+    li.appendChild(p);
+
+    //* delete iconu👇
+    const deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fas fa-trash");
+    li.appendChild(deleteIcon);
+
+
+
+
+
+    todoUl.appendChild(li);
+
+};
