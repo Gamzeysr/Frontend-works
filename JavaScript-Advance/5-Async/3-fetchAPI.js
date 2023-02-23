c4f3299924dd4b36c343cb41e4fba172f63f0552
Gamze <gamzeyasar35@icloud.com>
2023-02-23 11:37:20 +0300

    destructuring

--- JavaScript-Advance/5-Async/3-fetchAPI.js
+++ JavaScript-Advance/5-Async/3-fetchAPI.js
@@ -35,6 +35,7 @@ console.log("FETCH");
 
 // };
 
+// --------------------------------------------------------------
 
 // fetch("https://api.github.com/users")
 //     .then((res) => (res.json()))
@@ -62,3 +63,70 @@ console.log("FETCH");
 //     })
 //     .then((data) => console.log(data))
 //     .catch((hata) => console.log(hata));
+
+// ----------------------------------------------------------------
+//? 👇asenkron senkron olayına kısaca degindik tekrar burada
+
+// let userData;
+
+
+// fetch("https://api.github.com/users")
+//     .then((res) => {
+//         if (!res.ok) {
+//             throw new Error("something went wrong");
+//         }
+//         return res.json();
+//     })
+//     .then((data) => (userData = data))
+//     // datamı userrDataya aktardım👆 
+//     .catch((hata) => console.log(hata));
+// console.log(userData);
+// const userDiv = document.querySelector(".users")
+
+//! ✨Bunun sonucu 👆 Undefined döner çünkü; fetch api de promise baglı olarakdan bi asenkron yapı oldgundan kuyruga girer bu kuyrukta makro kısmına girer  ilk önce fetch yapısnın içine girmeden globaldeki userData calısır sonra fetch ın içerisine girer veri alır ama bir daha biz yazdırma yapmadgımız için undefined görürüz
+//!✨ ve fetch ın içini görmek için fonksiyona yazdırmamız lazım.Yani aşağıdaki gibi yapmalıyız👇👇
+
+// fetch("https://api.github.com/users")
+//     .then((res) => {
+//         if (!res.ok) {
+//             throw new Error("something went wrong")
+//         }
+//         return res.json();
+//     })
+//     .then((data) => updateDOM(data))
+//     .catch((hata) => console.log(hata));
+
+// const updateDOM = (users) => {
+//     const userDiv = document.querySelector(".users")
+//     // console.log(users);
+//     // artık elimde verilerim var artık verilerimi userDivine basıcam 
+//     users.forEach((user) => {
+//         // console.log(user);
+//         userDiv.innerHTML += `<h2>${user.login}</h2>
+//         <img src="${user.avatar_url}" width="300px" alt=""/>`
+//     });
+//     // 👆her bir itemin içine giriyorum bu sekilde 
+
+// }
+
+// 👆Bu yukarıdaki leri Desct. yöntemi ile kısaca yapabiliriz 👇
+
+fetch("https://api.github.com/users")
+    .then((res) => {
+        if (!res.ok) {
+            throw new Error("something went wrong")
+        }
+        return res.json();
+    })
+    .then((data) => update(data))
+    .catch((hata) => console.log(hata));
+
+const update = (users) => {
+    const userDiv = document.querySelector(".users")
+    users.forEach((user) => {
+        const { login, avatar_url } = user; //!destructuring
+        userDiv.innerHTML += `<h2>${login}</h2>
+        <img src="${avatar_url}" width="300px" alt=""/>`
+
+    })
+}
