8a840708b80f529a855622557f6489497db95574
Gamze <gamzeyasar35@icloud.com>
2023-03-10 11:11:24 +0300

    buton

--- React/rc03_module-css/src/component/buton/Buton.jsx
+++ React/rc03_module-css/src/component/buton/Buton.jsx
@@ -6,7 +6,7 @@ const Buton = ({ btn }) => {
     //👆data mın içinde her bir verimin butonu farklıbutonlar oldugundan buttonumu buraya cagırdım props ile su anda bu sekilde verimi almış oldum.
     return (
         <div className={ButonStyle.title}>
-            <button>{btn}</button>
+            <button >{btn}</button>
             {/* 👆app.js içindeki cekmiş oldugum buton veriyi bu componente ekrana rendr ettim  */}
         </div >
     );
