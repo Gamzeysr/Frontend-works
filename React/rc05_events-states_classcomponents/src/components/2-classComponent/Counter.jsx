d4449846066742377933cba563bae223160d2524
Gamze <gamzeyasar35@icloud.com>
2023-03-11 10:18:07 +0300

    Bootstrap

--- React/rc05_events-states_classcomponents/src/components/2-classComponent/Counter.jsx
+++ React/rc05_events-states_classcomponents/src/components/2-classComponent/Counter.jsx
@@ -1,4 +1,4 @@
-/* ================= CLASS COMPONENTS AND STATE ====================
+//* ================= CLASS COMPONENTS AND STATE ====================
 //* React'da Class-Component'ler ES6 class yapisina dayanmaktadir.
 //* Cok fazla boilerplate kod icermektedir.
 //* Ancak Class-Component'ler React'da state'leri barindiran ilk component yapisidir.
@@ -9,50 +9,51 @@
 //* constructor'in disinda state, setState() metodu ile degistilebilir.
 //* ====================================================================
 
-import { Component } from "react";
+// import { Component } from "react";
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
+// class Counter extends Component {
+//   constructor(props) {
+//     super(props);
 
-    //! count state'ine baslangic degeri atadik
-    this.state = {
-      count: props.count || 0,
-    };
-    //? increment metodunun Counter class'ina baglanmasi (bind)
-    this.increment = this.increment.bind(this);
-  }
-  increment() {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
+//     //! count state'ine baslangic degeri atadik
+//     this.state = {
+//       count: props.count || 0,
+//     };
+//     //? increment metodunun Counter class'ina baglanmasi (bind)
+//     this.increment = this.increment.bind(this);
+//   }
+//   increment() {
+//     this.setState({
+//       count: this.state.count + 1,
+//     });
+//   }
 
-  //! Yazmis oldugumuz metotlar default olarak classa baglanmaz.
-  //! Ancak, React built-in fonksiyonlari baglidir.
+//   //! Yazmis oldugumuz metotlar default olarak classa baglanmaz.
+//   //! Ancak, React built-in fonksiyonlari baglidir.
 
-  //* Bunun icin metotlarimizi ya constructor icerisinde baglamaliyiz yada otomatik baglamayi saglayan arrow fonksiyonlarini kullanmaliyiz.
-  decrement = () => {
-    this.setState({
-      count: this.state.count - 1,
-    });
-  };
+//   //* Bunun icin metotlarimizi ya constructor icerisinde baglamaliyiz yada otomatik baglamayi saglayan arrow fonksiyonlarini kullanmaliyiz.
+//   decrement = () => {
+//     this.setState({
+//       count: this.state.count - 1,
+//     });
+//   };
 
-  render() {
-    return (
-      <div className="container text-center mt-4">
-        <h1>CLASSFUL COMPONENTS</h1>
-        <h2 className="display-4 text-danger">COUNT:{this.state.count}</h2>
-        <button onClick={this.increment} className="btn btn-success">
-          INC
-        </button>
+//   render() {
+//     return (
+//       <div className="container text-center mt-4">
+//         <h1>CLASSFUL COMPONENTS</h1>
+//         <h2 className="display-4 text-danger">COUNT:{this.state.count}</h2>
+//         <button onClick={this.increment} className="btn btn-success">
+//           INC
+//         </button>
 
-        <button onClick={this.decrement} className="btn btn-danger">
-          DEC
-        </button>
-      </div>
-    );
-  }
-}
+//         <button onClick={this.decrement} className="btn btn-danger">
+//           DEC
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-export default Counter;
+// export default Counter;
+
--- React/rc05_events-states_classcomponents/src/index.js
+++ React/rc05_events-states_classcomponents/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
+// Bootstrap deki CSS linki bunu yüklememiz gerek 👇
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
