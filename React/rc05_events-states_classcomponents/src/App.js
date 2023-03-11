447315520eed3b20f5e8d9168afed8f0afb46060
Gamze <gamzeyasar35@icloud.com>
2023-03-11 09:52:02 +0300

    deleted

--- React/rc05_events-states_classcomponents/src/App.js
+++ React/rc05_events-states_classcomponents/src/App.js
@@ -1,6 +1,7 @@
-mport Events from "./components/1-events/Events";
+import Events from "./components/1-events/Events";
 import Counter from "./components/2-classComponent/Counter";
 import UseStateCounter from "./components/3-useStateHook/UseStateCounter";
+
 
 function App() {
   return (
