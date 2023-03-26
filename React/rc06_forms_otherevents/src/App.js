b8eb9e9b40071ccc8b96316207f4565e9d296356
Gamze <gamzeyasar35@icloud.com>
2023-03-26 17:16:48 +0300

    mouseevents

--- React/rc06_forms_otherevents/src/App.js
+++ React/rc06_forms_otherevents/src/App.js
@@ -1,16 +1,17 @@
 
 import './App.css';
 // import FormObject from './components/1-forms/FormObject';
-import KeyboardEvents from './components/2-keyboardClipboardEvent/KeyboardClipboard';
-// import MouseEvent from './components/3-mouseEvents/3-MouseEven';
+// import KeyboardEvents from './components/2-keyboardClipboardEvent/KeyboardClipboard';
+import MouseEvent from './components/3-mouseEvents/MouseEvent';
+
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
       {/* <FormObject /> */}
-      <KeyboardEvents />
-      {/* <MouseEvent /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
 
     </div>
   );
