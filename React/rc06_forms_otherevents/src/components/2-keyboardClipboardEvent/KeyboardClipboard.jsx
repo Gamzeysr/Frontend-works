4e23e552c2a5b3d85be19facee20dc1d655e7aaa
Gamze <gamzeyasar35@icloud.com>
2023-03-26 13:24:40 +0300

    keyboard

--- React/rc06_forms_otherevents/src/components/2-keyboardClipboardEvent/KeyboardClipboard.jsx
+++ React/rc06_forms_otherevents/src/components/2-keyboardClipboardEvent/KeyboardClipboard.jsx
@@ -4,18 +4,49 @@ import { useState } from "react";
 
 const KeyboardClipboard = () => {
     const [inputValue, setInputValue] = useState("");
-    return <div className="container text-center
+
+    const handleKeyDown = (e) => {
+        console.log(e.keyCode);
+        if (e.keyCode >= 48 && e.keyCode <= 57) {
+            alert("Please dont enter a number");
+            e.preventDefault()
+        }
+        e.keyCode === 13 && setInputValue("");
+    };
+    //! onKeyDown={handleKeyDown} yaparak klavye ye de bir tusa basıldıgında rakamları basılırsa eger yazma dedik bu sekilde
+
+
+
+    return (<div className="container text-center
     ">
         <h1>CLIPBOARD EVENTS </h1>
         <input className="form-control"
             type="text"
-            onChange={(e) => setInputValue(e.target.value)} />
-        {/* 👆setli olan onChangede yazılan kullanıcının inputa girilecek olan değeri*/}
+            value={inputValue}
+            //👆Bu value olmazsa inputa girilen deger silinmiyor o yuzden koyduk 
+            onChange={(e) => {
+                e.target.value = e.target.value.toLocaleUpperCase();
+                setInputValue(e.target.value)
+                {/* 👆setli olan onChangede yazılan kullanıcının inputa girilecek olan değeri*/ }
+            }}
+            onKeyDown={handleKeyDown}
+        />
+
 
 
         <p className="text-center mt-4">{inputValue}</p>
         {/* 👆girilen değeri bize kullanıcıya göstericek olan kısım  */}
+        <textarea
+            className="form-control"
+            name="area"
+            id="area"
+            rows="10"
+        >
 
-    </div>;
+        </textarea>
+
+    </div>
+    );
+
 };
 export default KeyboardClipboard;
