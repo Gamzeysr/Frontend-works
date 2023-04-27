ae5b5b97d935cd3779e723ce353a4a942648db3b
Gamze <gamzeyasar35@icloud.com>
2023-04-27 16:13:44 +0300

    url

--- React/rc09_useeffect-axios-example/src/pages/Home.jsx
+++ React/rc09_useeffect-axios-example/src/pages/Home.jsx
@@ -3,7 +3,13 @@ import TutorialList from "../components/TutorialList";
 import axios from "axios";
 
 const Home = () => {
-    const url = "## https://tutorials-api-cw.herokuapp.com/api/tutorials";
+    const url = "https://tutorials-api-cw.herokuapp.com/api/tutorials";
+
+    const getTutorials = async () => {
+        const data = await axios(url);
+        console.log(data);
+    };
+    getTutorials();
     return (
         <>
             <AddTutorial />
