487cbeb711178a3677be9bde60895117ba7daf6a
Gamze <gamzeyasar35@icloud.com>
2023-07-10 20:06:10 +0300

    b

--- React/rc16_context-api/context-example2/src/context/LoginProvider.jsx
+++ React/rc16_context-api/context-example2/src/context/LoginProvider.jsx
@@ -11,13 +11,11 @@ const LoginProvider = ({ children }) => {
   const [user, setUser] = useState({ email: "", password: "" });
 
   const values = { user, setUser };
-  //☝ statelerim  burada iki tane var ve daha sonrasında daha fazla olabilir bunun için bize kolaylık olsunn diye vulues adında bir değişkene atadık bunları 
+  //☝ statelerim  burada iki tane var ve daha sonrasında daha fazla olabilir bunun için bize kolaylık olsunn diye vulues adında bir değişkene atadık bunları
   // ve artık bu valuesları valuenin içine gönderebilirim
   // DiKKAT:DEĞİŞKEN seklinde gönderilen olunca kullanırken süslü paranteze alıyoruz unutma !! , FONNKSYİON OLUNCA () Sonua parantez koyarak kullanıyoruz .
   return (
-    <LoginContext.Provider value={values}>
-      {children}
-    </LoginContext.Provider>
+    <LoginContext.Provider value={values}>{children}</LoginContext.Provider>
   );
 };
 // ☝Bu sarmalladıogmız children her componentin içinde olan bi şey .
